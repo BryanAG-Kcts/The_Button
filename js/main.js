da870412492649a0beb30608c4e340c06641f161
Bryan David Alvarez Galvis <bryanalvarezg.kcts@gmail.com>
2023-07-18 10:46:36 -0500

    Updates

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+const $ = element => document.querySelector(element);
+const $body = document.body
+
+export const $theButton = $('.theButton');
+export const $theButtonBody = $theButton.querySelector("span")
+export const $showPercentaje = $('.showPercentaje');
+export const $maxValueFail = $('.maxValueFail');
