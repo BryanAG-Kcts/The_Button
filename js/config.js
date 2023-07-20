a66d556677ef420fd5c041d604ddba537c24d0e7
Bryan David Alvarez Galvis <bryanalvarezg.kcts@gmail.com>
2023-07-20 10:34:30 -0500

    Updates

--- js/config.js
+++ js/config.js
@@ -1,9 +1,10 @@
-import {$, $body, $mainDisplayGame, $showPercentaje, $maxValueButton, $maxValueFail} from "./main.js"
+import {$, $body, $mainDisplayGame, $showPercentaje, $maxValueButton, $maxValueFail, $theButton, $theButtonBody} from "./main.js"
 
 const $userConfig = $('.userConfig');
 const $modalContainer = $('.modalContainer');
 const $modalConfig = $('.modalConfig');
-const $modalConfigSub = $('.modalConfigSub');
+const $modalConfigSubGrid = $('.modalConfigSubGrid');
+const $modalConfigSubFlex = $('.modalConfigSubFlex');
 
 async function infoConfig() {
     const infoModalConfig = await fetch("./appeal/config.txt")
@@ -32,6 +33,10 @@ function modalComponent(infoModal) {
     $modalConfig.insertAdjacentHTML("beforeend", template);
     const $$modalOptionsContainer = document.querySelectorAll(".modalOptionsContainer");
 
+    //Colores del botón
+    $$modalOptionsContainer[0].addEventListener("click", () => {
+        buttonColorOptions(infoModal[1])
+    })
     //Modo oscuro
     $$modalOptionsContainer[1].addEventListener("click", () => $body.classList.toggle("bodyDarkMode"))
     //Ocultar - Mostrar Porcentaje
@@ -44,10 +49,36 @@ function modalComponent(infoModal) {
     $$modalOptionsContainer[4].addEventListener("click", () => toggleVisibleElement($maxValueFail))
 
     //atrás
-    $$modalOptionsContainer[7].addEventListener("click", () => toggleVisibleConfig())
+    $$modalOptionsContainer[6].addEventListener("click", () => toggleVisibleConfig())
 }
 
 $userConfig.addEventListener("click", () => toggleVisibleConfig())
+
+function buttonColorOptions(colorObject) {
+    let template = "";
+    for (let i = 0; i < colorObject.length; i++) {
+        template += /*html*/ `
+        
+            <span class="colorButtonOptions h-10 w-10 border-4 z-10 rounded-full" 
+            style="background: var(${colorObject[i].lightColor}); border-color : var(${colorObject[i].darkColor})"></span>
+        `
+    }
+
+    $modalConfigSubFlex.innerHTML = template;
+    const $$colorButtonOptions = document.querySelectorAll('.colorButtonOptions');
+
+    for (let i = 0; i < $$colorButtonOptions.length; i++) {
+        $$colorButtonOptions[i].addEventListener("click", () => buttonChangeColor(colorObject[i]))
+    }
+}
+
+function buttonChangeColor(colorButton) {
+    const buttonSelectColor = new Audio("./media/selection.mp3")
+    buttonSelectColor.play();
+
+    $theButton.style.setProperty("--base-Color", `var(${colorButton.darkColor})`);
+    $theButton.style.setProperty("--body-Color", `var(${colorButton.lightColor})`);
+}
 
 function toggleVisibleElement($element) {
     $element.classList.toggle("invisible");
@@ -58,10 +89,11 @@ function toggleVisibleConfig() {
     clickAudio.play();
 
     $modalContainer.classList.toggle("hidden");
-    $mainDisplayGame.classList.toggle("invisible")
+    $modalContainer.classList.toggle("flex");
+    $mainDisplayGame.classList.toggle("invisible");
 
     setTimeout(() => {
-        $modalConfig.classList.toggle("scale-0")
+        $modalConfig.classList.toggle("scale-0");
         $modalConfig.classList.toggle("scale-100"); 
     },10)
 }
